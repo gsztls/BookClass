b533f50be782dfe111f7bb69884e8983b6870644
ForConfidence <1072184387@qq.com>
2018-05-25 16:07:51 +0800

    2018.5.25

---
+++ Admin/AddNotice.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+
+public partial class Notice : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string Title = TxtTitle.Text;
+        string Text = TxtText.Text;
+        string Time = DateTime.Now.ToLongDateString();
+        if (Title == "" || Text == "")
+        {
+            Response.Write("alert('信息输入不完整！')");
+            return;
+        }
+        int JudgeNum = SqlHelper.ExecuteNonQuery(CommandType.Text,"INSERT INTO [BookClass].[dbo].[Notice](Title , Text , Time) VALUES ('"+Title+"','"+Text+"','"+Time+"')");
+        if (JudgeNum > 0)
+        {
+            Response.Write("<script>alert('添加成功！');window.location = 'ManageNotice.aspx';</script>");
+        }
+        else
+        {
+            Response.Write("<script>alert('添加失败！');</script>");
+            return;
+        }
+    }
+}
---
+++ Notice.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+
+public partial class Notice : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        DataSet DsNotice = SqlHelper.ExecuteDataset(CommandType.Text, "SELECT * FROM [BookClass].[dbo].[Notice]");
+        DataList1.DataKeyField = "ID";  //将主键存入到DataKeys集合当中，以便后面对某一条数据进行编辑。  
+        DataList1.DataSource = DsNotice.Tables[0];
+        DataList1.DataSourceID =null;
+        DataList1.DataBind();  
+
+    }
+    protected void DataList1_ItemCommand(object source, DataListCommandEventArgs e)
+    {
+        if (e.CommandName == "Select")
+        {
+            string ID = DataList1.DataKeys[e.Item.ItemIndex].ToString();
+            Response.Redirect("ShowNotice.Aspx?UrlID=" + System.Web.HttpContext.Current.Server.UrlEncode(ID) + "");          
+        }  
+
+    }
+}
